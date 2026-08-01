3000bdd04600061b53401e112b18c60be1e54817
Jaspal Rajput <jashpal900@gmail.com>
2026-08-01 11:54:01 +0530

    Remove debug logs

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const reportRoute = require("./routes/reports");
 const razorpayRoute = require("./routes/razorpay");
 
 // --- MONGODB CONNECTION ---
-console.log("ENV CHECK:", process.env.MONGO_URI);
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
